22d7063a2cf64a7d8789688e5de4db11d9d7b94b
jordanalexis6 <jordan.a.neill@gmail.com>
2021-08-12 17:13:20 -0500

    made the segments disapear from screen once it hits the border

--- snakegame.py
+++ snakegame.py
@@ -78,6 +78,10 @@ while True:
         head.goto(0, 0)
         head.direction = "stop"
 
+        # hide segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+
     # check for collision with food
     if head.distance(food) < 20:
         # move food to random spot
